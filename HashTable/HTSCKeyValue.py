1dcf28342035ffaf297e8cc9d46384ad96c15c7f
hemant <hjain1011@gmail.com>
2022-05-24 17:09:48 +0530

    Third edition

--- HashTable/HTSCKeyValue.py
+++ HashTable/HTSCKeyValue.py
@@ -3,7 +3,7 @@ import sys
 class HashTableSC(object):
     def __init__(self):
         self.table_size = 512
-        self.adj_list = [[] for _ in range(self.table_size)]
+        self.arr_list = [[] for _ in range(self.table_size)]
 
     def compute_hash(self, key):
         #  division method
@@ -17,32 +17,32 @@ class HashTableSC(object):
 
     def add(self, key, value):
         index = self.compute_hash(key)
-        self.adj_list[index].append((key, value))
+        self.arr_list[index].append((key, value))
 
     def get(self, key):
         index = self.compute_hash(key)
-        for pair in self.adj_list[index]:
+        for pair in self.arr_list[index]:
             if pair[0] == key:
                 return pair[1]
         return sys.maxsize
         
     def remove(self, key):
         index = self.compute_hash(key)
-        for pair in self.adj_list[index]:
+        for pair in self.arr_list[index]:
             if pair[0] == key:
-                self.adj_list[index].remove(pair)
+                self.arr_list[index].remove(pair)
             return True
         return False
 
     def print(self):
         for i in range(self.table_size):
-            if len(self.adj_list[i]) != 0:
-                for keyvalue in self.adj_list[i]:
+            if len(self.arr_list[i]) != 0:
+                for keyvalue in self.arr_list[i]:
                     print("Values of key [" , keyvalue[0],"] ::", keyvalue[1])
 
     def find(self, key):
         index = self.compute_hash(key)
-        for pair in self.adj_list[index]:
+        for pair in self.arr_list[index]:
             if pair[0] == key:
                 return True
         return False
